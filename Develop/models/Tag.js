a890ecc0ed1c733644d05d7a5228939588159e0c
Qu33nArlene <ahaskins93@yahoo.com>
2022-06-24 18:29:43 -0400

    added models

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -6,7 +6,15 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,      
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    }
   },
   {
     sequelize,
